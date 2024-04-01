bbed060fa9b962e3cbe8f0c34af903bad1928126
Tomasz Smyda <tomasz.smyda@gmail.com>
2024-04-01 23:54:56 +0200

    lab05 update

--- lab05/zad1.c
+++ lab05/zad1.c
@@ -1,26 +1,41 @@
-#include <bits/types/sigset_t.h>
-#include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
-
-void obsluga_v1(int signum, siginfo_t * si, void * p3) {
-  printf("Obsluga sygnalu v1:%d %d, war:%d\n", si -> si_pid, si->si_uid, si->si_value);
+#include <signal.h>
+#include <string.h>
+void handleSIGUSR1(int signum) {
+    printf("Otrzymano sygnał SIGUSR1\n");
 }
 
-int main(void) {
-  // signal(SIGUSR1, SIG_IGN);
-  // raise(SIGUSR1);
-  // while(1);
-  // return 0;
-  sigset_t set;
-  struct sigaction act;
-  printf("pid:%d\n", getpid());
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <ignore|handler|mask>\n", argv[0]);
+        return -1;
+    }
 
-  act.sa_sigaction=obsluga_v1;
-  sigemptyset(&act.sa_mask);
-  act.sa_flags = 4;
-  sigaction(SIGUSR1, &act, NULL);
-  return 0;
+    sigset_t sigset;
 
+    if (strcmp(argv[1], "ignore") == 0) {
+        signal(SIGUSR1, SIG_IGN);
+    } else if (strcmp(argv[1], "handler") == 0) {
+        signal(SIGUSR1, handleSIGUSR1);
+    } else if (strcmp(argv[1], "mask") == 0) {
+        sigemptyset(&sigset);
+        sigaddset(&sigset, SIGUSR1);
+        sigprocmask(SIG_BLOCK, &sigset, NULL);
+    } else {
+        fprintf(stderr, "Unknown param: %s\n", argv[1]);
+        return EXIT_FAILURE;
+    }
+
+    raise(SIGUSR1);
+
+    sigpending(&sigset);
+    if (sigismember(&sigset, SIGUSR1)) {
+        printf("widoczny");
+    } else {
+        printf("niewidoczny");
+    }
+
+    return 1;
 }
